6d0f0d3a3ee43b12dd45854d7d65843170f16541
ksigei <sigeikiprono4@gmail.com>
2022-12-01 02:17:56 +0300

    Create basic pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-
+    <div className={styles.container}>
+      <title>Create Next App</title>
+    </div>
   )
 }
